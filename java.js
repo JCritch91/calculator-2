ab1c643f73be0522301241472e3ee56aeb1c9755
Jamie <134149238+JCritch91@users.noreply.github.com>
2023-11-23 17:03:59 +0000

    CREATE STYLES FOR CALCULATOR

--- java.js
+++ java.js
@@ -1,36 +1,151 @@
-const equalsButton = document.querySelector('.equals')
-const resultText = document.querySelector('.result')
+const screenSum = document.querySelector('.screen-sum')
+const screenResult = document.querySelector('.screen-result')
+const buttonContainer = document.querySelector('.button-container')
+const numberbutton = document.querySelector('.number')
 
-let num1 = 1
-let num2 = 3
-let operator = '+'
+let num1 = ''
+let num2 = ''
+let operator = ''
+result = 'nothing'
 
+buttonContainer.addEventListener('mousedown', mouseDwn, false)
+buttonContainer.addEventListener('mouseup', mouseUp, false)
+
+buttonContainer.addEventListener('click', handleClick, false)
+
+function mouseDwn(e){
+    const { target } = e  
+    target.classList.add('mousedown')
+}
+
+function mouseUp(e){
+    const { target } = e  
+    target.classList.remove('mousedown')
+}
+
+function handleClick(e) {
+    const { target } = e    
+
+    if (target.classList.contains('number')) {
+        if (result !=='' && operator == ''){
+            fullReset()
+            
+        }
+        if (operator == ''){
+            num1 += target.id
+        }
+        else {
+            num2 += target.id
+        }
+    }
+
+    else if (target.classList.contains('decimal')) {
+        if (num1 !== '' && operator =='') {
+            if (num1.includes(target.id)) {
+                return
+            }
+            else{
+            num1 += target.id
+            }
+        }
+        else if (operator !== '') {
+            if (num2.includes(target.id)){
+                return
+            }
+            else {
+            num2 += target.id
+            }
+        }
+    }
+
+    else if (target.classList.contains('operator')) {
+        if (operator == '' && num1 !== ''){
+            operator = target.id
+        }
+        else if (operator !== '' && num2 !== '' ){
+            operate(num1, operator, num2)
+        }
+        else {
+            num1 = result 
+            operator = target.id
+            screenSum.textContent = `${num1} ${operator} ${num2}`
+        }
+    }
+
+    else if (target.classList.contains('equals')) {
+        if (num1 !=='' && num2 !==''){
+            operate(num1, operator, num2)
+        }
+        else {
+            return
+        }
+
+    }
+
+    else if (target.classList.contains('delete')) {
+        if (num1 == '' && operator == '' && num2 ==''){
+            return
+        }
+        else if (num2 !== ''){
+            num2 = num2.slice(0, -1)
+        }
+        else if (num2 =='' && operator !== ''){
+            operator = ''
+        }
+        else if (num2 =='' && operator ==''){
+            num1 = num1.slice(0, -1)
+            screenSum.textContent = `${num1} ${operator} ${num2}`
+        }
+    }
+
+    else if (target.classList.contains('clear')) {
+        fullReset()
+    }
+    if (num1==''){
+        return
+    }
+    else {
+        screenSum.textContent = `${num1} ${operator} ${num2}`
+    }
+
+}
 
 function add(num1, num2) {
-    resultText.textContent = num1 + num2
-    return num1 + num2
+    result = Number(num1) + Number(num2)
+    screenResult.textContent = parseFloat((result).toFixed(5))
+    reset()
+    return
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+     result = Number(num1) - Number(num2)
+     screenResult.textContent = parseFloat((result).toFixed(5))
+    reset()
+    return
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+    result = Number(num1) * Number(num2)
+    screenResult.textContent = parseFloat((result).toFixed(5))
+    reset()
+    return
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+    if (num2 == 0) {
+        screenResult.textContent = 'Cant Divide By Zero'
+    }
+    else{
+    result = Number(num1) / Number(num2)
+    screenResult.textContent = parseFloat((result).toFixed(5))
+    }
+    reset()
+    return 
 }
-
-equalsButton.addEventListener('click', () => {
-    operate(num1, operator, num2)
-}) 
 
 function operate(num1, operator, num2) {
     switch(operator) {
         case '+':
-
             add(num1, num2)
         break;
 
@@ -46,5 +161,22 @@ function operate(num1, operator, num2) {
             divide(num1, num2)
         break;
     }
+    return
+}
+
+function reset() {
+    num1 = ''
+     num2 = ''
+     operator = ''
+     
+}
+
+function fullReset() {
+    num1 = ''
+    operator = ''
+    num2 = ''
+    result = ''
+    screenResult.textContent = '0'
+    screenSum.textContent = ''
     return
 }
